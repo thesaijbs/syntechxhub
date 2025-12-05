0cd19d299d5ff21ee014df55f612970218a99765
thesaijbs <153373016+thesaijbs@users.noreply.github.com>
2025-12-05 21:40:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,85 @@
+// app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+const File = require('./models/file.model');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const startServer = async () => {
+  const mongod = await MongoMemoryServer.create();
+  const uri = mongod.getUri();
+
+  await mongoose.connect(uri);
+  console.log('Connected to In-Memory MongoDB');
+
+  const db = mongoose.connection;
+  db.on('error', (error) => console.error('MongoDB connection error:', error));
+
+
+  // Multer configuration
+  const storage = multer.memoryStorage();
+  const upload = multer({ storage: storage });
+
+  // Route for uploading a PDF file
+  app.post('/upload', upload.single('pdf'), async (req, res) => {
+    try {
+      const { originalname, buffer, mimetype } = req.file;
+
+      const file = new File({
+        name: originalname,
+        data: buffer,
+        contentType: mimetype,
+      });
+
+      await file.save();
+      res.status(201).send('File uploaded successfully.');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error uploading the file.');
+    }
+  });
+
+
+  // Route to display a list of uploaded files
+  app.get('/files', async (req, res) => {
+    try {
+      const files = await File.find();
+      res.send(files);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error retrieving files from the database.');
+    }
+  });
+
+  // Route to display an individual file based on its ID
+  app.get('/files/:id', async (req, res) => {
+    try {
+      const file = await File.findById(req.params.id);
+
+      if (!file) {
+        return res.status(404).send('File not found');
+      }
+
+      // Send the file data as a response
+      res.contentType(file.contentType);
+      res.send(file.data);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error retrieving the file from the database.');
+    }
+  });
+
+  app.use(express.static(__dirname));
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
